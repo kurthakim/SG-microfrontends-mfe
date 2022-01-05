8e24d7ba9da6fc91ad152f2999e37101398784be
Kurt Hakim <kurthakim@gmail.com>
2022-01-05 15:44:27 +0700

    changed text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi theree!</h1>
+      <h1>Hi thereee!</h1>
       <hr />
       <MarketingApp />
     </div>
